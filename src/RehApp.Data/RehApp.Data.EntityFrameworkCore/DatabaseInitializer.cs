11a647d21bed024723482a63927acec012b0207b
diukin <petr.diukin@yandex.ru>
2023-03-25 23:08:54 +0500

    Прокинул инициализацию городов (Екб, Мск) при старте проекта

--- src/RehApp.Data/RehApp.Data.EntityFrameworkCore/DatabaseInitializer.cs
+++ src/RehApp.Data/RehApp.Data.EntityFrameworkCore/DatabaseInitializer.cs
@@ -14,14 +14,33 @@ public static class DatabaseInitializer
         using var context = scope.ServiceProvider.GetService<ApplicationDbContext>()!;
         using var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>()!;
 
-        SeedDeveloperUser(userManager);
+        SeedCities(context);
+        SeedDeveloperUser(userManager, context);
 
         userManager.Dispose();
         context.Dispose();
         scope.Dispose();
     }
 
-    public static void SeedDeveloperUser(UserManager<ApplicationUser> userManager)
+    public static void SeedCities(ApplicationDbContext context)
+    {
+        var requiringInitialization = new List<City>()
+        {
+            new City { Name = "Екатеринбург", ShortName = "ekb", Latitude = 56.833332, Longitude = 60.583332 },
+            new City { Name = "Москва", ShortName = "msk", Latitude = 55.751244, Longitude = 37.618423 },
+        };
+
+        var existing = context.Cities.ToList();
+
+        var missingCities = requiringInitialization
+            .Where(x => !existing.Any(y => y.Name == x.Name))
+            .ToList();
+
+        context.Cities.AddRange(missingCities);
+        context.SaveChanges();
+    }
+
+    public static void SeedDeveloperUser(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
     {
         if (userManager.FindByNameAsync("developer").Result is not null) return;
 
@@ -29,7 +48,10 @@ public static class DatabaseInitializer
         {
             UserName = "developer",
             Email = "developer@rehapp.ru",
-            EmailConfirmed = true
+            FirstName = "Superuser",
+            LastName = "Developer",
+            EmailConfirmed = true,
+            City = context.Cities.FirstOrDefault()!
         };
 
         userManager.CreateAsync(user, "qwerty123").Wait();
